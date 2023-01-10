cd8cab05084f966677398bbd7475bf816869533c
Altynbek Orumbayev <aorumbayev@protonmail.com>
2023-01-10 03:10:35 +0100

    fix: babel error resolved

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  presets: [['env'], '@babel/preset-react', '@babel/preset-typescript'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
   env: {
     test: {
       plugins: ['dynamic-import-node'],
