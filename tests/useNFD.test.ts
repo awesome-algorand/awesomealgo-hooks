9f9aa7e1b199e8876b21c1ad29aa02dfe1cad020
Altynbek Orumbayev <aorumbayev@protonmail.com>
2023-01-10 03:40:46 +0100

    feat: testing deployment

--- tests/useNFD.test.ts
+++ tests/useNFD.test.ts
@@ -1,7 +1,18 @@
+import { renderHook } from '@testing-library/react-hooks';
 import useNfd from '../src/useNfd';
 
 describe('useNfd', () => {
   it('should be defined', () => {
     expect(useNfd).toBeDefined();
+  });
+
+  it('should update document title', () => {
+    let hook = null;
+
+    renderHook((props) => useNfd(props), {
+      initialProps: 'NRLA7VZ2YV6WOS2LBS3UK25DB463XRSNG63BYNQI6CSGGROFWVQ2EKBQSI',
+    });
+
+    expect(hook).toBeDefined();
   });
 });
